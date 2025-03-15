d4199653d865ea7e406e7efba8834d49f609b06b
Paolo <grep96@icloud.com>
2025-03-15 18:54:56 +0100

    ottimizzazione varie, miglioramento commenti
    ottimizzato il setup() per la fase di test, e migliorati e resi più pertinenti i commenti (riscrittura per una migliore comprensione)

--- main/main.ino
+++ main/main.ino
@@ -11,13 +11,13 @@ char ssid[] = "IoT - WIFI";
 char pass[] = "03GDVBH223";
 
 // Definizione pin per ESP32 Lolin S2 Mini
-#define MOISTURESENSORPIN 7    // Sensore di umidità
-#define REDLEDPIN 9            // elettrovalvola
-#define TRIGPIN 3              // Trigger del sensore ultrasuoni
-#define ECHOPIN 5              // Echo del sensore ultrasuoni
-#define LEDRGB = 11; //Led RGB per indicare se c'è acqua o meno nel serbatoio (in mancanza o in aggiunta ad alexa)
-#define LEDRGB2 = 12; //Led RGB per indicare se c'è acqua o meno nel serbatoio (in mancanza o in aggiunta ad alexa)
-#define PIEZO = 1; //piezoelettrico sonoro che abbiamo usato a lezione per indicare se c'è acqua o meno nel serbatoio (in mancanza o in aggiunta ad alexa)
+#define MOISTURESENSORPIN 7    //sensore di umidità
+#define REDLEDPIN 9            //elettrovalvola
+#define TRIGPIN 3              //trigger del sensore a ultrasuoni
+#define ECHOPIN 5              //echo del sensore a ultrasuoni
+#define LEDRGB = 11; //led RGB per indicare se c'è acqua o meno nel serbatoio (in mancanza o in aggiunta ad alexa)
+#define LEDRGB2 = 12; //led RGB per indicare se c'è acqua o meno nel serbatoio (in mancanza o in aggiunta ad alexa)
+#define PIEZO = 1; //piezo elettrico sonoro che abbiamo usato a lezione per indicare se c'è acqua o meno nel serbatoio (in mancanza o in aggiunta ad alexa)
 
 #define SOGLIA_UM 7000 
 
@@ -28,66 +28,62 @@ void checkMoisture() {
   Serial.print("Umidità: ");
   Serial.println(moistureValue);
     
-  Blynk.virtualWrite(V2, moistureValue); // Invia il valore dell'umidità a Blynk su V2
-
-  /*
-
-  Attiva il LED giallo solo se l'umidità è sotto la soglia (da tenere ?)
+  Blynk.virtualWrite(V2, moistureValue); //invia il valore dell'umidità a Blynk su V2
 
   if (moistureValue <= SOGLIA_UM) {
-     Blynk.virtualWrite(V5, 255); // LED GIALLO (Alexa_Terreno) acceso
+    digitalWrite(redLedPin, HIGH); //"accensione" elettrovalvola per irrigazione
+    delay(1000);
+    digitalWrite(redLedPin, LOW);
   } else {
-    Blynk.virtualWrite(V5, 0);   // LED GIALLO spento
+    digitalWrite(redLedPin, LOW);
   }
-    
-  */
 }
 
 void checkWaterLevel() {
   long duration;
   float distance;
     
-  // Generazione dell'impulso ultrasonico
+  //Generazione dell'impulso ultrasonico
   digitalWrite(TRIGPIN, LOW);
   delayMicroseconds(2);
   digitalWrite(TRIGPIN, HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIGPIN, LOW);
     
-  // Lettura della durata dell'eco
+  //Lettura della durata dell'eco
   duration = pulseIn(ECHOPIN, HIGH);
 
-  // Calcolo della distanza
+  //Calcolo della distanza
   distance = (duration * 0.0343) / 2;
     
   Serial.print("Distanza: ");
   Serial.print(distance);
   Serial.println(" cm");
     
-  // Invia la distanza a Blynk
+  //Invia la distanza a Blynk
   Blynk.virtualWrite(V1, distance);
 
-  // Controllo dei LED e del piezo
+  //Controllo dei LED e del piezo
   if (distance > 4) {
       Blynk.virtualWrite(V3, 255);   // LED display blynk Rosso ACCESO
       Blynk.virtualWrite(V4, 0);     // LED display blynk Verde SPENTO
-      Blynk.logEvent("alexa_alert", "Riempire contenitore di acqua"); // Attiva Alexa con messaggio
+      Blynk.logEvent("alexa_alert", "Riempire contenitore di acqua"); //attiva Alexa con messaggio
 
       digitalWrite(LEDRGB, HIGH);  // LED RGB Rosso
       digitalWrite(LEDRGB2, LOW);  // LED RGB Verde spento
-      digitalWrite(PIEZO, HIGH);   // Piezoelettrico suona
+      digitalWrite(PIEZO, HIGH);   // piezo elettrico suona
   } else {
       Blynk.virtualWrite(V3, 0);     // LED display blynk Rosso SPENTO
       Blynk.virtualWrite(V4, 255);   // LED display blynk Verde ACCESO
 
       digitalWrite(LEDRGB, LOW);   // LED RGB Rosso spento
       digitalWrite(LEDRGB2, HIGH); // LED RGB Verde acceso
-      digitalWrite(PIEZO, LOW);    // Piezoelettrico non suona
+      digitalWrite(PIEZO, LOW);    // piezo elettrico non suona
   }
 }
 
 void setup() {
-  // Configurazione dei pin
+  //Configurazione dei pin
   pinMode(REDLEDPIN, OUTPUT);
   pinMode(TRIGPIN, OUTPUT);
   pinMode(ECHOPIN, INPUT);
@@ -99,22 +95,15 @@ void setup() {
 
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
 
-  timer.setInterval(5000L, checkMoisture);    // Controlla l'umidità dopo 5 secondi -> x "do-while finale"
-  timer.setInterval(5000L, checkWaterLevel);  // Controlla il livello dell'acqua dopo 5 secondi -> x "do-while finale"
+  //ciclo do-while, prima parte = do
+  timer.setInterval(checkMoisture);    // primo controllo umidità terreno  alla partenza
+  timer.setInterval(checkWaterLevel);  // primo controllo livello dell'acqua alla partenza
 }
 
 void loop() {
-    Blynk.run();
-    timer.run();
-    
-    //Irrigazione temporizzata, redLedPin = elettrovalvola -> versione finale ogni 24 ore (?)
-    if (moistureValue > SOGLIA_UM) {
-        digitalWrite(redLedPin, HIGH);
-        delay(3000);
-        digitalWrite(redLedPin, LOW);
-        delay(10000); // Attende 10 secondi prima di ripetere il ciclo, ma noi metteremo 24 ore (ovvero 86.400.000)
-    } else {
-        digitalWrite(redLedPin, LOW);
-        delay(10000); // Attende 10 secondi prima di ripetere il ciclo, ma noi metteremo 24 ore (ovvero 86.400.000)
-    }
-}
+  Blynk.run();
+  timer.run();
+
+  //ciclo do-while, seconda parte = while | definitivo -> ogni 24 ore = 86400000L (?)
+  timer.setInterval(10000L, checkMoisture);
+  timer.setInterval(10000L, checkWaterLevel);
